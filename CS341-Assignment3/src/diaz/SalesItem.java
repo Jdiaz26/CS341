e05c23d08ed39d7215ef390649a65f6e4d5a4c3a
jeff0 <jeff0@DESKTOP-E3GNVAP>
2025-10-16 21:18:55 -0700

    Assignment 3

---
+++ CS341-Assignment3/src/diaz/SalesItem.java
@@ -0,0 +1,28 @@
+package diaz;
+
+public class SalesItem {
+	private String name;
+	private double price;
+	private int quantity;
+	
+	//Constructor
+	public SalesItem (String n, double p, int q) {
+		name = n;
+		price = p;
+		quantity = q;
+	}
+	
+	//Getters
+	//Gets total price of item based on quantity
+	public double getTotal() {
+		return price * quantity;
+	}
+	
+	//toString
+	public String toString() {
+		return name + " $" + price + " x " + quantity + " = $ " + getTotal();
+	}
+	
+	
+
+}
---
+++ CS341-Assignment3/src/diaz/SalesSlip.java
@@ -0,0 +1,35 @@
+package diaz;
+
+import java.util.ArrayList;
+
+public class SalesSlip {
+	private ArrayList<SalesItem> list;
+	
+	public SalesSlip() {
+		list = new ArrayList<SalesItem>();
+	}
+	
+	public void addItem(SalesItem item) {
+		list.add(item);
+	}
+	
+	public double getTotal() {
+	    double total = 0;
+	    for (int i = 0; i < list.size(); i++) {
+	        SalesItem item = list.get(i); 
+	        total = total + item.getTotal();
+	    }
+	    return total;
+	}
+	
+	public String toString() {
+	    String all = "";
+	    for (int i = 0; i < list.size(); i++) {
+	        SalesItem item = list.get(i);
+	        all = all + item.toString() + "\n";
+	    }
+	    return all;
+	}
+	
+
+}
